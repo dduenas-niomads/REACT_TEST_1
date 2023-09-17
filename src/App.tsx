2283c3970b8dcdb302189c0925fe1aaa7d61a14a
Gissela <dduenas@niomads.com>
2023-09-16 19:20:23 -0500

    First commit. Please, enjoy!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialList = [
+	{ id: '1', name: 'daniel' }
+];
+
+const App = () => {
+	const [list, setList] = React.useState(initialList)
+	const [name, setName] = React.useState('')
+	
+  	function handleAdd() {
+		if (name != '') {
+			const newList = list.concat({ name, id: uuidv4() })
+			setList(newList)
+			setName('')
+		} else {
+			alert('Insert text')
+		}
+	}
+
+	function handleChange(event: any) {
+		if (event?.target.value) setName(event?.target.value)
+	}
+
+	function deleteLi(event: any) {
+		if (event) {
+			const element = document.getElementById(event.target.parentElement.id)
+			if (element) {
+				element.remove()
+			}
+		}
+	}
+
+	return (
+		<div>
+			<AddItem
+				name={name}
+				onChange={handleChange}
+				onAdd={handleAdd}
+			/>
+
+			<List list={list} deleteLi={deleteLi}/>
+		</div>
+  	)
 }
 
-export default App;
+const AddItem = ({ name, onChange, onAdd }: { name: string, onChange: any, onAdd: any }) => (
+	<div>
+		<input type="text" value={name} onChange={onChange} />
+		<button type="button" onClick={onAdd}>
+			New element
+		</button>
+	</div>
+)
+
+const List = ({ list, deleteLi }: { list: any, deleteLi: any}) => (
+	<ul>
+		{list.map((item:any) => (
+			<li id={item.id}
+				key={item.id}>
+				<button onClick={deleteLi}>
+					{item.name}
+				</button>
+			</li>
+		))}
+	</ul>
+)
+
+export default App
